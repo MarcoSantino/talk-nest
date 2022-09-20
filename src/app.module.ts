3525aa8ef507eaeb423326e8cc4d8c0ca0f25202
Marco Elia Santino <marco.elia.santino@accenture.com>
2022-09-20 14:30:19 +0200

    feat: serve static page

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { GraphqlModule } from './graphql/graphql.module';
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [GraphqlModule],
+  imports: [
+    GraphqlModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
